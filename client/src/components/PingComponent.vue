5ef3f67efaf4c5790f4627e3b434c621b84c6d59
Brian Jenkins <bj@jenkinz.com>
2022-05-24 11:05:23 -0700

    Get client-server communication working

--- client/src/components/PingComponent.vue
+++ client/src/components/PingComponent.vue
@@ -1,13 +1,30 @@
 <script>
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'PingComponent',
   setup() {},
   data() {
     return {
-      msg: 'Hello!',
+      msg: 'Default',
     };
+  },
+  created() {
+    this.getMessage();
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/ping';
+      axios
+        .get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
 });
 </script>
